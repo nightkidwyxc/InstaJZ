a42ed7b6585acea5b00b6ce1e0349da965df2b0d
nightkidwyxc <nightkidwyxc@Weizhous-MacBook-Pro.local>
2020-05-16 23:32:03 -0400

    CRUD

--- Insta/urls.py
+++ Insta/urls.py
@@ -17,8 +17,13 @@ from django.contrib import admin
 from django.urls import include
 from django.urls import path
 
-from Insta.views import HelloWorld
+from Insta.views import HelloWorld, PostDeleteView, PostUpdateView, PostsView, PostDetailView, PostCreateView
 
 urlpatterns = [
-    path('', HelloWorld.as_view(), name='hellowprld')
+    path('', HelloWorld.as_view(), name='helloworld'),
+    path('posts/', PostsView.as_view(), name='posts'),
+    path('post/<int:pk>', PostDetailView.as_view(), name='post_detail'),
+    path('post/new/', PostCreateView.as_view(), name='make_post'),
+    path('post/update/<int:pk>', PostUpdateView.as_view(), name='post_update'),
+    path('post/delete/<int:pk>', PostDeleteView.as_view(), name='post_delete')
 ]
