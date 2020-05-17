ba1e7b8a6f8edb41595667f298d795f6c0b15c67
nightkidwyxc <nightkidwyxc@Weizhous-MacBook-Pro.local>
2020-05-17 15:24:40 -0400

    login, logout, and signup

--- Insta/models.py
+++ Insta/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from imagekit.models import ProcessedImageField
 
 from django.urls import reverse
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class Post(models.Model):
@@ -17,3 +18,12 @@ class Post(models.Model):
     
     def get_absolute_url(self):
         return reverse("post_detail", args=[str(self.id)])
+
+class InstaUser(AbstractUser):
+    profile_pic = ProcessedImageField(
+        upload_to='static/images/posts',
+        format='JPEG',
+        options={'quality':100},
+        blank=True,
+        null=True
+        )
--- Insta/views.py
+++ Insta/views.py
@@ -21,6 +21,10 @@ from Insta.models import Post
 from django.views.generic import CreateView, UpdateView, DeleteView
 from django.urls import reverse, reverse_lazy
 
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+from Insta.forms import CustomUserCreationForm
+
 class HelloWorld(TemplateView):
     template_name = 'test.html'
 
@@ -32,10 +36,11 @@ class PostDetailView(DetailView):
     model = Post 
     template_name = 'post_detail.html'
 
-class PostCreateView(CreateView):
+class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post 
     template_name = 'post_create.html'
     fields = '__all__'
+    login_url = 'login'
 
 class PostUpdateView(UpdateView):
     model = Post 
@@ -46,4 +51,14 @@ class PostDeleteView(DeleteView):
     model = Post 
     template_name = 'post_delete.html'
     success_url = reverse_lazy("posts")
+
+# class SignUp(CreateView):
+#     form_class = UserCreationForm
+#     template_name = 'signup.html'
+#     success_url = reverse_lazy("login")
+
+class SignUp(CreateView):
+    form_class = CustomUserCreationForm
+    template_name = 'signup.html'
+    success_url = reverse_lazy("login")
     
